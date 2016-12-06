d174e32642c86c99ce0749024f222cd39a5167c6
drzel <shayolden@hotmail.com>
2016-12-06 15:29:05 +1100

    install rubocop, make codebase compliant

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,7 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Wellwish
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
+    # Settings in config/environments/* take precedence over those specified
+    # here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
