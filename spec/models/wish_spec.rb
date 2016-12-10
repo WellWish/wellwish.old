0f8761a4bc3699ae32480c6480dfb359ae5197ba
drzel <shayolden@hotmail.com>
2016-12-10 13:01:07 +1100

    Add wish and user association specs

--- spec/models/wish_spec.rb
+++ spec/models/wish_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Wish, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:user) }
 end
