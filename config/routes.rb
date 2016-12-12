d8210a6f6fb062c4d23cd75eba371a5191cfe98e
drzel <shayolden@hotmail.com>
2016-12-13 01:51:03 +1100

    root path directs to Wish#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :wishes
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "welcome#index"
+  root "wishes#index"
+  devise_for :users
+  resources :wishes
 end
