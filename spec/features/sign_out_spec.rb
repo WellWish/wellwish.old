6ab186ad5f221a1ff110a36393135b8b24df8eaf
drzel <shayolden@hotmail.com>
2016-12-14 13:51:04 +1100

    small refactor sign_out_spec

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -2,11 +2,9 @@ require "rails_helper"
 
 feature "Signing out" do
   scenario "user signs out" do
-    create :user, email:                 "joebloggs@email.com",
-                  password:              "password"
+    create :user, email: "joebloggs@email.com", password: "password"
 
     sign_in_with email: "joebloggs@email.com", password: "password"
-
     click_link "Sign out"
     expect(page).to have_text "Signed out successfully."
   end
