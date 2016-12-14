52af9c91d2293c937cd3c18d74022e55bd171195
drzel <shayolden@hotmail.com>
2016-12-14 11:48:42 +1100

    No need to supply password_confirmation in new instances of user factory

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 feature "Signing out" do
   scenario "user signs out" do
     create :user, email:                 "joebloggs@email.com",
-                  password:              "password",
-                  password_confirmation: "password"
+                  password:              "password"
 
     sign_in_with email: "joebloggs@email.com", password: "password"
-    click_on "Sign out"
+
+    click_button "Sign out"
     expect(page).to have_text "Signed out successfully."
   end
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -13,8 +13,7 @@ feature "Signing up" do
   context "when email is taken" do
     scenario "user is notified that the email address is taken" do
       create :user, email:                 "joebloggs@email.com",
-                    password:              "password",
-                    password_confirmation: "password"
+                    password:              "password"
 
       sign_up_with email: "joebloggs@email.com", password: "password"
       expect(page).to have_text "Email has already been taken"
@@ -33,6 +32,7 @@ feature "Signing up" do
       sign_up_with email: "joebloggs@email.com",
                    password: "password",
                    password_confirmation: "drowssap"
+
       expect(page).to have_text "Password confirmation doesn't match"
     end
   end
